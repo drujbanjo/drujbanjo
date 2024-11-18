0f179c44fffbcc87d874a82524c1d026a62c6d0b
drujbanjo <drujbanjo@gmail.com>
2024-11-18 10:26:31 +0500

    fix: browserTagsToggle, add: queryClientDevTools. (#12)

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -4,9 +4,13 @@ import { queryClient } from "@/lib"
 import { ProvidersProps } from "@/types"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { FC } from "react"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export const Providers: FC<ProvidersProps> = ({ children }) => {
 	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	)
 }
