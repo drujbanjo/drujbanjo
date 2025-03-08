3ad1225b4bbd8debc12a5224590886af651727f9
drujbanjo <drujbanjo@gmail.com>
2025-03-08 11:03:41 +0500

    rewrite: home page. (#19)

--- client/src/app/(root)/(home)/page.tsx
+++ client/src/app/(root)/(home)/page.tsx
@@ -19,7 +19,7 @@ import styles from "./page.module.scss"
 import Link from "next/link"
 import { BookMarked, Notebook } from "lucide-react"
 import { stringToDate } from "@/utility"
-import { useGetProjects } from "@/hooks"
+import { useGetPosts, useGetProjects } from "@/hooks"
 
 const HomePage: FC = () => {
 	return (
@@ -28,6 +28,7 @@ const HomePage: FC = () => {
 			<FadeContent>
 				<Masthead />
 				<Projects />
+				<Posts />
 			</FadeContent>
 		</div>
 	)
@@ -39,7 +40,7 @@ const Masthead: FC = () => {
 	return (
 		<section className={styles.masthead}>
 			<Container>
-				<h5>
+				<h5 className={styles.masthead_subtitle}>
 					<ShinyText text="Не теряй время в поисках информации" speed={2} />
 				</h5>
 				<h1 className={styles.masthead_title}>
@@ -53,14 +54,12 @@ const Masthead: FC = () => {
 					чтобы ничего на забывать. Все это вы можете использовать здесь
 				</p>
 				<div className={styles.buttons_group}>
-					<Magnet>
-						<Button size={"xl"} asChild>
-							<Link href={"blog"}>
-								<BookMarked />
-								<span>Мой блог</span>
-							</Link>
-						</Button>
-					</Magnet>
+					<Button size={"xl"} asChild>
+						<Link href={"blog"}>
+							<BookMarked />
+							<span>Мой блог</span>
+						</Link>
+					</Button>
 					<Button size={"xl"} variant={"outline"} asChild>
 						<Link href={"notebook"}>
 							<Notebook />
@@ -107,6 +106,46 @@ const Projects: FC = () => {
 				<Magnet>
 					<Button size={"lg"} asChild variant={"accent"} className={styles.projects_button}>
 						<Link href={"projects"}>Все проекты</Link>
+					</Button>
+				</Magnet>
+			</Container>
+		</section>
+	)
+}
+
+const Posts: FC = () => {
+	const { data: posts } = useGetPosts()
+	return (
+		<section>
+			<Container>
+				<h2>Посты</h2>
+				<ul className={styles.posts_list}>
+					{posts?.map(
+						post =>
+							post.idInt <= 3 && (
+								<li key={post.idInt}>
+									<Card id={post.id}>
+										<Link href={`posts/${post.id}`} className={styles.post_link}>
+											<div>
+												<CardHeader>
+													<CardTitle>{post.name}</CardTitle>
+												</CardHeader>
+												<CardContent>
+													<CardDescription>{post.description}</CardDescription>
+												</CardContent>
+											</div>
+											<CardFooter>
+												<CardDescription>{stringToDate(post.createdAt)}</CardDescription>
+											</CardFooter>
+										</Link>
+									</Card>
+								</li>
+							)
+					)}
+				</ul>
+				<Magnet>
+					<Button size={"lg"} asChild variant={"accent"} className={styles.posts_button}>
+						<Link href={"blog"}>Все посты</Link>
 					</Button>
 				</Magnet>
 			</Container>
